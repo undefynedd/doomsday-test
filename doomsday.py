5c731bd4d397286dc3da458a26ed7fa054a591f3
undefynedd <64054522+undefynedd@users.noreply.github.com>
2022-05-17 16:46:43 -0700

    Add files via upload

--- doomsday.py
+++ doomsday.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 import calendar
 import random
+import time
 
 question_num="10"
 
@@ -18,32 +19,35 @@ except:
     response=False
 
 correct=0
+times=[]
 dayname=""
 
 
 def ask(x):
     date = datetime.datetime.strptime(x, "%Y-%m-%d")
+    start_time = time.time()
     answer = input("\033[1m"+date.strftime("%Y %B %d")+"\n\033[0m")
+    times.append(time.time()-start_time)
     dayname = calendar.day_name[date.weekday()]
+    if answer.lower() == dayname.lower():
+        print('good job')
+    else:
+        print('wrong, it was '+dayname+".")
     return answer.lower() == dayname.lower()
 
 if response.ok and response:
     for i in response.iter_lines():
         if ask(i.decode("utf-8")):
-            print('good job')
             correct += 1
-        else:
-            print('wrong, it was '+dayname+".")
+            
 else:
     file = open("dates", "r")
     dates=file.readlines()
     print('returned bad, opening backup file (not random)')
     for i in range(int(question_num)):
         if ask(dates[random.randint(0,len(dates))].strip('\n')):
-            print('good job')
             correct += 1
-        else:
-            print('wrong, it was '+dayname+".")
 
 print("Your score: "+str(correct)+"/"+question_num)
+print("Your average time was: "+str(sum(times)/len(times))+".")
         
